9a197c7dc914f4433e6e9a248b832ede915b840e
谢宗匠 <zongjiang.xie@dianrong.com>
2018-01-25 21:32:34 +0800

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -36,7 +36,7 @@ export default class HomePage extends React.Component {
         // 若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: jsSign.ticket, // 必填，公众号的唯一标识
         timestamp: jsSign.timestamp, // 必填，生成签名的时间戳
-        nonceStr: jsSign.nonceStr, // 必填，生成签名的随机串
+        nonceStr: jsSign.noncestr, // 必填，生成签名的随机串
         signature: jsSign.signature, // 必填，签名，见附录1
         jsApiList: [
           'onMenuShareTimeline',
